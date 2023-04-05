04ca1ba205bd5f8dee4a6eb165f756566ecc67b4
Mulugeta Achiso <anem1351@gmail.com>
2023-04-05 01:00:12 -0700

    task 3

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-// Import the required modules
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import controllerRouting from './routes/index';
 
-// Import the routes
-const routes = require('./routes/index');
-
-// Create the Express app
 const app = express();
-
-// Parse JSON requests
-app.use(bodyParser.json());
-
-// Load all routes from the file routes/index.js
-app.use('/', routes);
-
-// Define the port to listen on
 const port = process.env.PORT || 5000;
 
-// Start the server
+app.use(express.json());
+
+controllerRouting(app);
+
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
